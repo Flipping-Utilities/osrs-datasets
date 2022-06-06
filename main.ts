fcd612af1237e4bf57c6a88cc8d02f79152e571c
Antony Garand <9059487+AntonyGarand@users.noreply.github.com>
2022-06-06 10:29:15 -0400

    Added missing hellhound

--- main.ts
+++ main.ts
@@ -3,7 +3,10 @@ import {
   fetchGEItems,
   TestGeItems,
 } from "./scripts/extractors/extract-ge-items";
-import { dumpAllWikiPages } from "./scripts/dumpers/page-content";
+import {
+  dumpAllWikiPages,
+  dumpWikiPageById,
+} from "./scripts/dumpers/page-content";
 import {
   dumpGEItemPageList,
   dumpAllItemPageList,
@@ -17,13 +20,17 @@ import { dumpAllSets, extractAllSets } from "./scripts/extractors/extract-sets";
 import { dumpAllRecipes } from "./scripts/extractors/extract-recipes";
 
 import { dumpAllShops } from "./scripts/extractors/extract-shops";
-import { dumpAllMonsters, extractAllMonsters } from "./scripts/extractors/extract-monsters";
+import {
+  dumpAllMonsters,
+  extractAllMonsters,
+} from "./scripts/extractors/extract-monsters";
 
 // dumpWikiPageList();
 // dumpAllWikiPages();
 // dumpRedirectList();
 // TestGeItems();
-// dumpGEItems();
+// dumpGEItemPageList();
+dumpGEItems();
 // dumpAllItemPageList();
 // dumpAllSets();
 // dumpAllRecipes();
@@ -33,4 +40,5 @@ import { dumpAllMonsters, extractAllMonsters } from "./scripts/extractors/extrac
 
 // dumpMonstersPageList();
 // dumpAllMonsters();
-extractAllMonsters();
+// extractAllMonsters();
+// dumpWikiPageById(357068);
--- scripts/dumpers/page-content.ts
+++ scripts/dumpers/page-content.ts
@@ -8,7 +8,7 @@ import {
 } from "../wiki/request";
 
 interface WikiPageResponse {
-  title: number;
+  title: string;
   revid: number;
   displaytitle: string;
   text: {
@@ -36,34 +36,39 @@ export async function dumpAllWikiPages(): Promise<void> {
     }
 
     const currentPage = WikiPageList[i] as WikiPageSlim;
-    const redirects = WikiRequest.getRedirectsToPage(currentPage.pageid);
-    const response = await WikiRequest.query<{ parse: WikiPageResponse }>({
-      action: "parse",
-      pageid: currentPage.pageid.toString(),
-      format: "json",
-      prop: "properties|wikitext|displaytitle|subtitle|revid|text",
-    }).catch((e) => console.error(e));
-    if (!response) continue;
-    const result = response.parse as WikiPageResponse;
 
-    const newPage: WikiPageWithContent = {
-      pageid: currentPage.pageid,
-      pagename: currentPage.title,
-      title: result.displaytitle,
-      displaytitle: result.displaytitle,
-      revid: result.revid,
-      redirects: await redirects,
-      properties: result.properties.map((p) => ({
-        name: p.name,
-        value: p["*"],
-      })),
-      content: result.text["*"],
-      rawContent: result.wikitext["*"],
-    };
-
-    writeFileSync(
-      `${WIKI_PAGES_FOLDER}/${newPage.pageid}.json`,
-      JSON.stringify(newPage)
-    );
+    await dumpWikiPageById(currentPage.pageid);
   }
+}
+
+export async function dumpWikiPageById(pageId: number) {
+  const redirects = WikiRequest.getRedirectsToPage(pageId);
+  const response = await WikiRequest.query<{ parse: WikiPageResponse }>({
+    action: "parse",
+    pageid: pageId.toString(),
+    format: "json",
+    prop: "properties|wikitext|displaytitle|subtitle|revid|text",
+  }).catch((e) => console.error(e));
+  if (!response) return;
+  const result = response.parse as WikiPageResponse;
+
+  const newPage: WikiPageWithContent = {
+    pageid: pageId,
+    pagename: result.title,
+    title: result.displaytitle,
+    displaytitle: result.displaytitle,
+    revid: result.revid,
+    redirects: await redirects,
+    properties: result.properties.map((p) => ({
+      name: p.name,
+      value: p["*"],
+    })),
+    content: result.text["*"],
+    rawContent: result.wikitext["*"],
+  };
+
+  writeFileSync(
+    `${WIKI_PAGES_FOLDER}/${newPage.pageid}.json`,
+    JSON.stringify(newPage)
+  );
 }
